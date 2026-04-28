fd87c7ec8ee4f503f7b9b3acf6f39b839c77f49e
JannePiiroinen <amk1005446@student.hamk.fi>
2026-04-28 18:30:46 +0300

    added mongodb-connection and env.example, also removed node_modules

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+require ('dotenv').config();
 
 app.use(express.json());
 
@@ -12,3 +14,16 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+// Mongodb connection
+
+const DBUSER = process.env.DBUSER;
+const DBPASSWORD = process.env.DBPASSWORD;
+const CLUSTERNAME = process.env.CLUSTERNAME;
+const APPNAME = process.env.APPNAME;
+
+const URI = `mongodb+srv://${DBUSER}:${DBPASSWORD}@${CLUSTERNAME}.pjm9jvj.mongodb.net/?appName=${APPNAME}`;
+
+mongoose.connect(URI)
+.then((result) => console.log("Connected to DB"))
+.catch((err) => console.log(err))
